8ab80b4f7611debce0f41ec594c790e7028c1f67
GorkaBidaa <gbidaguren@gmail.com>
2026-03-04 13:22:06 +0100

    archivos de plantillas

---
+++ clasificarItemsNuevos.py
@@ -0,0 +1,69 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import getopt
+import sys
+import numpy as np
+import pandas as pd
+import sklearn as sk
+import imblearn
+import pickle
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+
+
+model=""
+p="./"
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('ARGV   :',sys.argv[1:])
+    try:
+        options,remainder = getopt.getopt(sys.argv[1:],'p:m:f:h',['path=','model=','testFile=','h'])
+    except getopt.GetoptError as err:
+        print('ERROR:',err)
+        sys.exit(1)
+    print('OPTIONS   :',options)
+
+    for opt,arg in options:
+        if opt in ('-p','--path'):
+            p = arg
+        elif opt in ('-f', '--file'):
+            f = arg
+        elif opt in ('-m', '--model'):
+            m = arg
+        elif opt in ('-h','--help'):
+            print(' -p modelAndTestFilePath \n -m modelFileName -f testFileName\n ')
+            exit(1)
+
+    
+    if p == './':
+        model=p+str(m)
+        iFile = p+ str(f)
+    else:
+        model=p+"/"+str(m)
+        iFile = p+"/" + str(f)
+        
+
+    #Abrir el fichero .csv con las instancias a predecir y que no contienen la clase y cargarlo en un dataframe de pandas para hacer la prediccion
+    y_test=pd.DataFrame()
+    testX = pd.read_csv(iFile)
+
+    print(testX.head(5))
+    clf = pickle.load(open(model, 'rb'))
+    predictions = clf.predict(testX)
+    probas = clf.predict_proba(testX)
+    y_test['preds'] = predictions
+    predictions = pd.Series(data=predictions, index=testX.index, name='predicted_value')
+    results_test = testX.join(predictions, how='left')
+    
+    print(results_test)
+    
+
+    
